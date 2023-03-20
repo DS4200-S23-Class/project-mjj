45e4b36e158efa9fe8289d96915c80db99877cc0
jethro-ronald-lee <99052024+jethro-ronald-lee@users.noreply.github.com>
2023-03-20 17:08:16 -0400

    altered files

--- data/Untitled.R
+++
@@ -1,4 +0,0 @@
-enrollment <- read_csv("Enrollment.csv", na = c("-3", "-5", "-6", "-8", "-9", 
-                                                "-11"))
-yearly_pubs_gender2 <- pivot_wider(yearly_pubs_gender, names_from = gender, 
-                                   values_from = author_counts, values_fill=0)
