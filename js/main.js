e00428905b1d26dc0d15d7f18fb85f19ddbd8def
Jane <ytzheng2003@gmail.com>
2023-04-02 22:27:47 -0400

    Added functions for tab

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
+function openTab(evt, tabName) {
+  // Declare all variables
+  let i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+};
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
+// Function that allows user to filter data by year through drop down menus
+function updateYear (menuId) {
+    // retrieve the year chosen by the user from the drop down menu
+    let yearMenu = document.getElementById(menuId);
+    let selected_year = yearMenu.options[yearMenu.selectedIndex].text;
+    return selected_year;
+  };
+
 // Function that allows user to filter data by year through drop down menus
 function updateYear (menuId) {
     // retrieve the year chosen by the user from the drop down menu
